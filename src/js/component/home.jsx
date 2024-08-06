204f08bcbeb25674b7429d3508c4718dca13bbdc
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2024-08-06 01:29:31 +0000

    counter without css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,34 @@
 import React from "react";
+import ReactDOM from 'react-dom';
+import Card from "./card";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+const Home = ({seconds}) => {
+
+	return (		
+		<div>
+			<div className="container-fluid d-flex justify-content-center gap-2 bg-dark">
+						
+						<div className="card bg-black text-light">
+							<div className='card-body'>
+								<i className="fa-solid fa-clock"></i>
+							</div>
+						</div>
+
+						<Card number = {Math.floor(seconds % 1000000 / 10000)}/>
+						<Card number = {Math.floor(seconds % 100000 / 10000)}/>
+						<Card number = {Math.floor(seconds % 10000 / 1000)}/>
+						<Card number = {Math.floor(seconds % 1000 / 100)}/>
+						<Card number = {Math.floor(seconds % 100 / 10)}/>
+						<Card number = {seconds % 10}/>
+			</div>
+			
+			
+
 		</div>
+
+		
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,61 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let intervalID = null;
+let counter = 0;
+
+function updateCounter(){
+    counter++;
+    root.render(<MainComponent/>)
+}
+
+const MainComponent = () => {
+    return (<>
+        <Home seconds={counter}/>
+
+            <label>Starting Value</label>
+            <input type="number" onChange={(event)=>{
+                counter = event.target.value;
+                root.render(<MainComponent/>)
+            }}/>
+
+            <div className="p-2 d-flex gap-2">
+                <button onClick={()=>{
+                    intervalID = setInterval(updateCounter,1000);
+                }} className="btn btn-success">
+                        Start
+                </button>
+            </div>
+
+            <button onClick={()=>{
+                clearInterval(intervalID);
+                intervalID = null;
+                root.render(<MainComponent/>)
+            }} className="btn btn-danger ">
+                Stop
+            </button>
+
+            <button onClick={()=>{
+                if(intervalID){
+                    intervalID = null;
+                    counter = 0;
+                    root.render(<MainComponent/>)
+                }
+                else {
+                    counter = 0;
+                    root.render(<MainComponent/>)
+                }
+            }} className="btn btn-warning">
+                Reset
+            </button>
+        
+    </>)
+}
+
+let root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<MainComponent/>)
+
+
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+
 
