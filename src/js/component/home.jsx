734b738e51cff81432c8870d451e3cba3b679305
Luis Guerrero <158310573+LuisGuerrero7@users.noreply.github.com>
2024-08-13 01:07:51 +0000

    todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,12 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import Card from "./card";
+import ToDoList from "./lista";
 
 
 //create your first component
-const Home = ({seconds}) => {
-
-	return (		
-		<div>
-			<div className="container-fluid d-flex justify-content-center gap-2 bg-dark">
-						
-						<div className="card bg-black text-light">
-							<div className='card-body'>
-								<i className="fa-solid fa-clock"></i>
-							</div>
-						</div>
-
-						<Card number = {Math.floor(seconds % 1000000 / 10000)}/>
-						<Card number = {Math.floor(seconds % 100000 / 10000)}/>
-						<Card number = {Math.floor(seconds % 10000 / 1000)}/>
-						<Card number = {Math.floor(seconds % 1000 / 100)}/>
-						<Card number = {Math.floor(seconds % 100 / 10)}/>
-						<Card number = {seconds % 10}/>
-			</div>
-			
-			
-
+const Home = () => {
+	return (
+		<div className="container text-center">
+			<ToDoList/>
 		</div>
 
 		
